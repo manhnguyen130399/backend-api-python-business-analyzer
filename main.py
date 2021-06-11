4685f8c08d81dcdccb217bb27802a4b31f4b7f69
Manh Nguyen <manhnguyen130399@gmail.com>
2021-06-11 11:53:35 +0700

    fix

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from flask import Flask, redirect, url_for, request
 import numpy as np
 import json
 from flask_cors import CORS
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -295,4 +296,4 @@ def getCanSlim():
     "total": df.loc[0, 'C'] + df.loc[0, 'A']}
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5001)
+    app.run(host='0.0.0.0',port=os.environ.get('PORT', 5000))
